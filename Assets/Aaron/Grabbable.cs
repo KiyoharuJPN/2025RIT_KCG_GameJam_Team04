e7464e6baec24df3f078a4b13253d9ccf55fceec
Awesomecake <aaronodellbush@gmail.com>
2025-05-23 22:13:10 -0400

    testing dragging

--- Assets/Aaron/Grabbable.cs
+++ Assets/Aaron/Grabbable.cs
@@ -4,14 +4,17 @@ public class Grabbable : MonoBehaviour
 {
     [SerializeField] private LayerMask pointerFinger;
     [SerializeField] private LayerMask thumb;
+    [SerializeField] private GameObject pointerTarget;
 
     private CircleCollider2D circleCollider2D;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         circleCollider2D = GetComponent<CircleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,10 +23,12 @@ public class Grabbable : MonoBehaviour
         if (circleCollider2D.IsTouchingLayers(pointerFinger) && circleCollider2D.IsTouchingLayers(thumb))
         {
             spriteRenderer.color = Color.red;
+            transform.parent = pointerTarget.transform;
         }
         else
         {
             spriteRenderer.color = Color.green;
+            transform.parent = null;
         }
     }
 }
