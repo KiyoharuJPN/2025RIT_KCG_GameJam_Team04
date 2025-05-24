f6d41bd55444a445fd750f51aa3e94b245fb341b
kiyoharuJPN <1905088203@qq.com>
2025-05-24 14:14:34 +0900

    bottle

---
+++ Assets/Kiyoharu/Script/BottleItemsBase.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class BottleItemsBase : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Kiyoharu/Script/GrabbableItem.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class GrabbableItem : MonoBehaviour
+{
+    [SerializeField] private LayerMask pointerFinger;
+    [SerializeField] private LayerMask thumb;
+    [SerializeField] private GameObject pointerTarget;
+    [SerializeField] private GameObject thumbTarget;
+
+    private CircleCollider2D circleCollider2D;
+    private SpriteRenderer spriteRenderer;
+    private Bottle Bottleobj = null;
+    private Rigidbody2D rigidbody2D;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (circleCollider2D.IsTouchingLayers(pointerFinger) && circleCollider2D.IsTouchingLayers(thumb))
+        {
+            spriteRenderer.color = Color.red;
+
+            Vector3 betweenFingers = (pointerTarget.transform.position + thumbTarget.transform.position) / 2;
+
+            transform.position = betweenFingers;
+        }
+        else
+        {
+            if (Bottleobj != null)
+            {
+                Bottleobj.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 3;
+                Bottleobj.JoinBottle(gameObject);
+                //Destroy(GetComponent<GrabbableItem>());
+            }
+            spriteRenderer.color = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bottle"))
+        {
+            Bottleobj = collision.GetComponent<Bottle>();
+            Debug.Log("new");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bottle"))
+        {
+            Bottleobj = null;
+        }
+    }
+}
