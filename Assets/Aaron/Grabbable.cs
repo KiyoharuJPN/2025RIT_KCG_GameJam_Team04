23b20548ff5c903ac757ef0f386d7442c699225d
Awesomecake <aaronodellbush@gmail.com>
2025-05-23 22:50:32 -0400

    pushed teleport grab test

--- Assets/Aaron/Grabbable.cs
+++ Assets/Aaron/Grabbable.cs
@@ -5,24 +5,30 @@ public class Grabbable : MonoBehaviour
     [SerializeField] private LayerMask pointerFinger;
     [SerializeField] private LayerMask thumb;
     [SerializeField] private GameObject pointerTarget;
+    [SerializeField] private GameObject thumbTarget;
 
     private CircleCollider2D circleCollider2D;
     private SpriteRenderer spriteRenderer;
-    private SpringJoint2D springJoint2D;
+    private Rigidbody2D rigidbody2D;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         circleCollider2D = GetComponent<CircleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        springJoint2D = GetComponent<SpringJoint2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (circleCollider2D.IsTouchingLayers(pointerFinger) && circleCollider2D.IsTouchingLayers(thumb))
         {
             spriteRenderer.color = Color.red;
+
+            Vector3 betweenFingers = (pointerTarget.transform.position + thumbTarget.transform.position) / 2;
+
+            transform.position = betweenFingers;
         }
         else
         {
